3296921878e1ca18a66195a478a89de45200569c
WPZ <wangpeizhuo_37@163.com>
2023-01-23 12:26:53 +0800

    Add files via upload

--- run_CEFCON.sh
+++ run_CEFCON.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
-echo "Please run this script in the folder of $0"
 
 exp="./data/example/exp_hESC_hvg1000.csv"
-prior="./prior_data/network_human.csv"
+prior="./prior_data/network_human.csv.zip"
 DE="./data/example/DE_hESC_hvg1000.csv"
+TFs="./prior_data/hs_hgnc_tfs_lambert2018.txt"
 
-echo python CEFCON.py --input_expData $exp --input_priorNet $prior --input_genesDE $DE --cuda 0 --repeats 3 --edge_threshold_param 8 --out_dir ./out_test
+python CEFCON.py --input_expData $exp --input_priorNet $prior --input_genesDE $DE --cuda 3 --repeats 3 --edge_threshold_param 8 --out_dir ./out_test --TFs $TFs
