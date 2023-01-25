bfe0b094edeb486eb12cc4d76bfd6e255eccab91
WPZ <wangpeizhuo_37@163.com>
2023-01-26 00:47:13 +0800

    Update run_CEFCON.sh

--- run_CEFCON.sh
+++ run_CEFCON.sh
@@ -5,4 +5,4 @@ prior="./prior_data/network_human.csv.zip"
 DE="./data/example/DE_hESC_hvg1000.csv"
 TFs="./prior_data/hs_hgnc_tfs_lambert2018.txt"
 
-python CEFCON.py --input_expData $exp --input_priorNet $prior --input_genesDE $DE --cuda 3 --repeats 3 --edge_threshold_param 8 --out_dir ./out_test --TFs $TFs
+cefcon --input_expData $exp --input_priorNet $prior --input_genesDE $DE --cuda 3 --repeats 3 --edge_threshold_param 8 --out_dir ./out_test --TFs $TFs
